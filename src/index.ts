862a4dd0bd7b9f8696ba719a83bd749ac08b8c0b
Ryota Ohnishi <ryotaimc@gmail.com>
2022-10-02 18:43:53 -0600

    updated main logic

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import { fromSSO } from "@aws-sdk/credential-providers";
 import { loadSharedConfigFiles } from "@aws-sdk/shared-ini-file-loader";
-import { prompt } from "inquirer";
-import { exit } from "process";
+import inquirer from "inquirer";
+import { exec } from "child_process";
+
+const getRegionInput = async (): Promise<string> => {
+  const prompt = inquirer.createPromptModule();
+  const { region } = await prompt({
+    type: "input",
+    name: "region",
+    message: "input aws region"
+  })
+  return region;
+}
 
 interface AwsCredentialConfig {
   AWS_ACCESS_KEY_ID: string;
   AWS_SECRET_ACCESS_KEY: string;
-  AWS_SESSION_TOKEN: string;
+  AWS_SESSION_TOKEN: string | undefined;
   AWS_REGION: string;
 }
 
@@ -15,27 +25,38 @@ export const setAwsSsoCredential = async (): Promise<{
   accountId: string;
 }> => {
   const configs = await loadSharedConfigFiles();
-  const { profile } = await prompt([
-    {
-      type: "list",
-      name: "profile",
-      message: "select local profile for operation",
-      choices: Object.keys(configs.configFile),
-    },
-  ]);
-  const region = configs.configFile[profile].region;
-  process.env.AWS_REGION = region;
-  console.log(`AWS_REGION is set to ${region}`);
+  const prompt = inquirer.createPromptModule();
+  const { profile } = await prompt({
+    type: "list",
+    name: "profile",
+    message: "select local profile",
+    choices: Object.keys(configs.configFile)
+  });
   const credentialProvider = fromSSO({
     profile: profile,
   });
-  const credential = await credentialProvider().catch((e) => {
-    console.error(e);
-    return Promise.reject(e);
+  const localCredential = await credentialProvider().catch(async (e) => {
+    if (e.name === "CredentialsProviderError") {
+      console.log(`CredentialProviderError: invoking SSO login process`);
+      const loginProcess = exec(`aws sso login --profile ${profile}`);
+      await new Promise((resolve) => {
+        loginProcess.on("close", resolve);
+      })
+      console.log(`retrying credential set process`);
+      return await credentialProvider();
+    } else {
+      console.error(e);
+      throw e;
+    }
   });
-  process.env.AWS_ACCESS_KEY_ID = credential.accessKeyId;
-  process.env.AWS_SECRET_ACCESS_KEY = credential.secretAccessKey;
-  process.env.AWS_SESSION_TOKEN = credential.sessionToken;
+  const localRegion = configs.configFile[profile].region;
+  const credential: AwsCredentialConfig = {
+    AWS_ACCESS_KEY_ID: localCredential.accessKeyId,
+    AWS_SECRET_ACCESS_KEY: localCredential.secretAccessKey,
+    AWS_SESSION_TOKEN: localCredential.sessionToken,
+    AWS_REGION: typeof localRegion === "string" ? localRegion : await getRegionInput()
+  }
+  process.env = { ...process.env, ...credential };
   console.log(`${profile} credential set`);
   return {
     profileName: profile,
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,13 @@
 import { setAwsSsoCredential } from "../src/index";
+import { test, jest } from "@jest/globals";
 
-(async () => {
-  await setAwsSsoCredential();
-})();
+const USER_INPUT_TEST_MS = 60000;
+
+/**
+ * Local test only
+ * TODO: Update for CI test
+ */
+test("test main function ()", async () => {
+  jest.setTimeout(USER_INPUT_TEST_MS);
+  const result = await setAwsSsoCredential();
+})
